c69553d7a522643b819fa68af642a33e6f14c45c
Dhruva Vasudev Loke <asd.dhruva@outlook.com>
2017-09-24 13:27:55 +0000

    Implemented the __init__ function for analyser

--- sentiments/analyzer.py
+++ sentiments/analyzer.py
@@ -5,11 +5,28 @@ class Analyzer():
 
     def __init__(self, positives, negatives):
         """Initialize Analyzer."""
-
-        # TODO
+        posf = open(positives, "r")
+        negf = open(negatives, "r")
+        self.posw = set()
+        self.negw = set()
+        for line in posf.readlines():
+            if line[0] == ';':
+                continue
+            else:
+                self.posw.add(line.strip("\n"))
+                #print(line.strip("\n"))
+        for line in negf.readlines():
+            if line[0] == ';':
+                continue
+            else:
+                self.negw.add(line.strip("\n"))
+                #print(line.strip("\n"))
+        posf.close()
+        negf.close()
+        
 
     def analyze(self, text):
         """Analyze text for sentiment, returning its score."""
-
-        # TODO
+        print(self.posw.issuperset({text}))
+        print(self.negw.issuperset({text}))
         return 0
